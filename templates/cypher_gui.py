b9cf8ee2fa1605aaf2f569f233446ed2665265b8
Joe <jeusgao@163.com>
2020-12-02 09:45:34 +0800

    update app

--- templates/cypher_gui.py
+++ templates/cypher_gui.py
@@ -131,7 +131,7 @@ def _match():
     }, index=[0])
 
     _cypher = build_cypher(list(df.iterrows())[0][1], action='M')
-    st.text(f'!!!match BOOM!!! {_cypher}')
+    st.markdown('#### !!! Cypher BOOM !!! * Check cypher scripts below, copy or run it. *' + '\n\n\t' + _cypher)
 
     _submit = st.button('RUN')
     if _submit:
